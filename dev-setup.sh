cbe15933fd14b6afd0a4b6786932d1571e8c80b8
Dave Schinkel <dschinkel@gmail.com>
2024-07-07 22:00:11 -0500

    Update dev-setup.sh
    Various updates and added 1password

--- dev-setup.sh
+++ dev-setup.sh
@@ -7,16 +7,22 @@ echo "Following cannot be installed with Brew"
 echo "Giphy Capture: App Store"
 echo "Theine: App Store"
 echo "Microsoft Office (OneNote, etc.): OneNote: App Store;  MS Office (other): log into MS's site"
-echo "Code Together"
-echo "oh-my-zsh: https://ohmyz.sh"
+# echo "Code Together"
 
-echo "Installing oh-my-zsh"
-get https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -
+Echo "Enable showing hidden files in OS X (need to log out and back in for this to take effect"
+defaults write com.apple.finder AppleShowAllFiles TRUE
 
 echo "Installing brew..."
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+Echo "Create .zprofile file"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/dave/.zprofile
+
 eval "$(/opt/homebrew/bin/brew shellenv)"
+
+echo "Installing oh-my-zsh"
+echo "oh-my-zsh: https://ohmyz.sh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 echo "Installing Browsers"
 brew install --cask google-chrome
@@ -24,10 +30,10 @@ brew install --cask brave-browser
 
 echo "Installing Communicaton Tools"
 brew install --cask zoom
-brew install --cask discord
+b# rew install --cask discord
 brew install --cask slack
 brew install --cask miro
-brew install --cask gather
+# brew install --cask gather
 
 echo "Installing Languages"
 brew install node
@@ -39,20 +45,23 @@ echo "Installing Dev Tools..."
 brew install docker
 brew install git
 brew install yarn
-brew install --cask insomnia
+# brew install pnpm
+# Brew install rpm
+# brew install --cask insomnia
+brew install --cask postman
 brew install --cask tuple
-brew install --cask pop
+# brew install --cask pop
 brew install --cask sublime-text
 brew install --cask jetbrains-toolbox
 brew install --cask intellij-idea
-brew install --cask webstorm
-brew install --cask visual-studio-code
-brew install --cask pgadmin4
+# brew install --cask visual-studio-code
+# brew install --cask pgadmin4
 get --no-check-certificate http://install.ohmyz.sh -O - | sh
 
 echo "Installing Various Utilities"
 brew install --cask iterm2
 brew install wget
-brew install --cask spectacle
-brew install --cask cleanmymac
+brew install --cask rectangle
+# brew install --cask cleanmymac
 brew install --cask obs
+brew install --cask 1password
