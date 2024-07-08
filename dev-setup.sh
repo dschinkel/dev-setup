7f22fd28ed10df6b39be13c4ff3ad7fd4a2dab7a
Dave Schinkel <dschinkel@gmail.com>
2024-07-07 20:04:53 -0500

    Update and rename deve-setup.sh to dev-setup.sh
    rename setup file

--- dev-setup.sh
+++ dev-setup.sh
@@ -1,5 +1,5 @@
 # Homebrew Script for OSX
-# To execute: save, then cd to the director that containts this file and run `chmod +x ./setup-pc-work.sh` then `./setup-pc-work.sh`
+# To execute: save, then cd to the director that containts this file and run `chmod +x ./dev-setup.sh` then `./setup-pc-work.sh`
 
 # just comment out the ones I don't want if the job / client doesn't need it
 
